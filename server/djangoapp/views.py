bc939e0f97cae8598897077b0015b64258a7bef1
Ritish1441 <ritishsharrma@gmail.com>
2022-04-03 12:49:37 +0000

    new

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
